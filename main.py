7e78fbf40b34a95ac261ea147752a1812d9cba11
Aashish Jain <37073516+Aashish095@users.noreply.github.com>
2023-04-19 23:40:36 +0530

    Update main.py

--- main.py
+++ main.py
@@ -16,41 +16,51 @@ list_2 = [
     },
 ]
 
-def merge_lists(list_1, list_2):
-    """
-    Merge the information from list_1 and list_2 to create a new list, which has all the information about each student 
-    from both lists in one single dict. 
+def merge_lists(list_1, list_2) -> list:
+    # Initialize an empty list to hold the merged data
+    merged = []
     
-    Args:
-    - list_1: (list) a list of dictionaries representing student information
-    - list_2: (list) a list of dictionaries representing student information
-    
-    Returns:
-    - merged_list: (list) a list of dictionaries representing the merged information for each student
-    
-    """
-    merged_list = []
-    
-    # Merge the two lists into a single list of dictionaries
-    merged_dict = {}
-    for d in list_1 + list_2:
-        id = d.get('id')
-        if id in merged_dict:
-            # If the ID is already in the merged dict, update it with the new values
-            merged_dict[id].update(d)
-        else:
-            # Otherwise, add a new entry to the merged dict
-            merged_dict[id] = d
-    
-    # Convert the merged dict back into a list
-    for id, d in merged_dict.items():
-        merged_list.append(d)
-    
-    return merged_list
-
+    # Iterate through each item in list_1
+    for item1 in list_1:
+        found = False
         
+        # Iterate through each item in list_2
+        for item2 in list_2:
+            # If the "id" key in item1 matches the "id" key in item2
+            if item1["id"] == item2["id"]:
+                # Merge the information in item1 and item2
+                item2.update(item1)
+                # Append the merged item to the list
+                merged.append(item2)
+                # Set the found flag to True and break out of the loop
+                found = True
+                break
+        
+        # If the item1 id was not found in list_2, append it to the merged list
+        if not found:
+            merged.append(item1)
     
-    # return list_3
+    # Iterate through each item in list_2
+    for item2 in list_2:
+        found = False
+        
+        # Iterate through each item in list_1
+        for item1 in list_1:
+            # If the "id" key in item2 matches the "id" key in item1
+            if item2["id"] == item1["id"]:
+                # Set the found flag to True and break out of the loop
+                found = True
+                break
+        
+        # If the item2 id was not found in list_1, append it to the merged list
+        if not found:
+            merged.append(item2)
+    
+    # Print the merged list (optional)
+    print(merged)
+    
+    # Return the merged list
+    return merged
 
 
 list_3 = merge_lists(list_1, list_2)
